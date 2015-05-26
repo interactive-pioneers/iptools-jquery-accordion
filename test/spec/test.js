763a6b0eef67fd3ee916470422629fcb8f7b5324
Georg Meyer <gm@interactive-pioneers.de>
2015-05-26 22:58:08 +0200

    Replaced $ with jQuery

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,12 +6,15 @@
     var expandableModule = null;
 
     describe('init', function() {
+
       beforeEach(function() {
-        expandableModule = $('.expandable_module').expandableModule();
+        expandableModule = jQuery('.expandable_module').expandableModule();
       });
+
       it('expected to construct object', function() {
         return expect(expandableModule).to.exist;
       });
+
     });
   });
 })();
