a50bb8ebc3e34e54f380b7d893961d17abd5876a
Georg Meyer <gm@interactive-pioneers.de>
2015-05-26 21:20:48 +0200

    Add basic test

---
+++ test/spec/test.js
@@ -0,0 +1,17 @@
+'use strict';
+/* jshint undef: false */
+(function() {
+  describe('iptoolsExpandableModule', function() {
+
+    var expandableModule = null;
+
+    describe('init', function() {
+      beforeEach(function() {
+        expandableModule = $('.expandable_module').expandableModule();
+      });
+      it('expected to construct object', function() {
+        return expect(expandableModule).to.exist;
+      });
+    });
+  });
+})();
